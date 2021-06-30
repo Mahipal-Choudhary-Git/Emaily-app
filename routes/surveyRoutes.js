9686b70ba9a2a0d9b9e6313e147fa8f222ebd35c
Mahipal Choudhary <dev.maipal@gmail.com>
2021-06-30 14:57:20 +0530

    server side sendgrid-email setup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,19 +1,40 @@
 import { Router } from "express";
 import { requireCredits, requireLogin } from "../middlewares";
-import surveyModel from "../models/Survey";
+import surveyModel from "../models/Survey.js";
+import Mailer from "../services/Mailer.js";
+import surveyTemplate from "../services/templates/surveyTemplate.js";
 const surveyRoutes = Router();
 
-surveyRoutes.post("/api/survey", requireLogin, requireCredits, (req, res) => {
-    const { title, recipients, body, subject } = req.body;
-    const survey = new surveyModel({
-        title,
-        body,
-        subject,
-        recipients: recipients
-            .split(",")
-            .map((email) => ({ email: email.trim() })),
-        _user: req.user.id,
-    });
+surveyRoutes.get("/api/survey/thanks", (req, res) => {
+    res.send("Thanks for Voting!");
 });
+
+surveyRoutes.post(
+    "/api/survey",
+    requireLogin,
+    requireCredits,
+    async (req, res) => {
+        const { title, recipients, body, subject } = req.body;
+        const survey = new surveyModel({
+            title,
+            body,
+            subject,
+            recipients: recipients
+                .split(",")
+                .map((email) => ({ email: email.trim() })),
+            _user: req.user.id,
+        });
+        try {
+            const mailer = new Mailer(survey, surveyTemplate(survey));
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            res.send(user);
+        } catch (error) {
+            res.status(422).send(error);
+        }
+    }
+);
 
 export default surveyRoutes;
